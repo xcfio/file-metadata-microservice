b22722b70936c7f8f1e43c48b44aed1eb8dbcc40
SoftwareX Plus <119097812+xcfio@users.noreply.github.com>
2024-06-20 09:56:24 +0600

    add html form

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,23 @@ import cors from "cors"
 
 const app = express().use(cors({ optionsSuccessStatus: 200 }))
 
-app.get("/", (_req, res) => res.send("Hello World"))
+const htmlForm = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Upload Form</title>
+</head>
+<body>
+    <form action="/upload" method="post" enctype="multipart/form-data">
+        <label for="upfile">Choose a file to upload:</label>
+        <input type="file" id="upfile" name="upfile">
+        <input type="submit" value="Upload File">
+    </form>
+</body>
+</html>
+`
+
+app.get("/", (_req, res) => res.send(htmlForm))
 app.post("/api/fileanalyse", multer().any(), (req, res) => {
     const file = (req.files as Array<Express.Multer.File>).shift()!
     res.json({
