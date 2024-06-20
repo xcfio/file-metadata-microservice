99d3c072071006b0c16fcc5b2b2f6052de6a807e
SoftwareX Plus <119097812+xcfio@users.noreply.github.com>
2024-06-20 09:25:30 +0600

    add logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from "express"
+import multer from "multer"
 import cors from "cors"
 
 const app = express().use(cors({ optionsSuccessStatus: 200 }))
 
 app.get("/", (_req, res) => res.send("Hello World"))
+app.post("/api/fileanalyse", multer().single("file"), (req, res) => {
+    res.json({
+        name: req.file?.originalname,
+        type: req.file?.mimetype,
+        size: req.file?.size
+    })
+})
 
 app.listen(3000, () => console.log("Server is running"))
